cf1abe2b8dbb82912c32f19df6fafa01dba27bdb
Darren Chin <chindarren@ucla.edu>
2024-11-21 10:33:39 -0800

    More changes to visualize data

--- src/view_data.py
+++ src/view_data.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def view_data():
-    data = np.load(sys.argv[1])
+def view_data(data):
     n_nodes = (data.shape[1] - 1) // 2
     print(f"n_nodes={n_nodes}\ndata=\n{data}")
     for i in range(n_nodes):
         plt.scatter(data[:,1+2*i], data[:,2+2*i], s=2, label=f"Node {i+1}")
-    plt.title("Movement of Rope")
+    plt.title("Collected Positions of Nodes")
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
     plt.legend()
-    
+
     plt.savefig("./output/view_data/image")
 
+
 if __name__ == "__main__":
-    view_data()
+    view_data(np.load(sys.argv[1]))
